a6d9fcb54a09e96a4e47ad51d7540bd33bad88e2
peti <petikoch@gmail.com>
2015-12-11 15:38:11 +0100

    cleanup example

--- src/main/java/ch/petikoch/examples/rxjava/threading/SingleThreadedByDefault.java
+++ src/main/java/ch/petikoch/examples/rxjava/threading/SingleThreadedByDefault.java
@@ -43,6 +43,7 @@ public class SingleThreadedByDefault {
         };
 
         fastCustomerArrivalStream
+                .onBackpressureBuffer()
                 .observeOn(Schedulers.io())
                 .subscribe(slowCustomerServingAction);
 
